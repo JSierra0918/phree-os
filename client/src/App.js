c635b11355e312c43a4ac3a8e04d0f86c49f334e
a-ravitz <adam.ravitz@gmail.com>
2019-07-01 21:02:29 -0400

    auth0 is in place

--- client/src/App.js
+++ client/src/App.js
@@ -9,20 +9,38 @@ import StorePage from './pages/StorePage';
 import ManagePage from './pages/ManagePage';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+import NavBar from "./components/Navbar";
+import Profile from "./components/Profile";
+import PrivateRoute from "./components/PrivateRoute";
+
+
+
 
 function App() {
   return (
+    // <div className="App">
+    //   <Router>
+    //     <Switch>
+    //       <Route exact path="/" component={LoginPage} />
+    //       <Route exact path="/SignIn" component={SignIn} />
+    //       <Route exact path="/SignUp" component={SignUp} />
+    //       <Route exact path="/store" component={StorePage} />
+    //       <Route exact path="/manage" component={ManagePage} />
+    //     </Switch>
+    //   </Router>
+
+    // </div>
     <div className="App">
+      {/* New - use BrowserRouter to provide access to /profile */}
       <Router>
+        <header>
+          <NavBar />
+        </header>
         <Switch>
-          <Route exact path="/" component={LoginPage} />
-          <Route exact path="/SignIn" component={SignIn} />
-          <Route exact path="/SignUp" component={SignUp} />
-          <Route exact path="/store" component={StorePage} />
-          <Route exact path="/manage" component={ManagePage} />
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
         </Switch>
       </Router>
-
     </div>
   );
 }
---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+import { Route } from "react-router-dom";
+import { useAuth0 } from "../react-auth0-wrapper";
+
+const PrivateRoute = ({ component: Component, path, ...rest }) => {
+  const { isAuthenticated, loginWithRedirect } = useAuth0();
+
+  useEffect(() => {
+    const fn = async () => {
+      if (!isAuthenticated) {
+        await loginWithRedirect({
+          appState: { targetUrl: path }
+        });
+      }
+    };
+    fn();
+  }, [isAuthenticated, loginWithRedirect, path]);
+
+  const render = props => <Component {...props} />;
+
+  return <Route path={path} render={render} {...rest} />;
+};
+
+export default PrivateRoute;
