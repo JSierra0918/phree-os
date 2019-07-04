33cf10fa3b8134adc6890494e30bdc5f5a8eb409
Jorge Sierra <3d.jorgesierra@gmail.com>
2019-07-03 22:10:54 -0400

    converted signup component to a container and has working input values

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Input, FormBtn } from './Bootstrap/Form';
+import Axios from 'axios';
 
-function SignUp() {
-    return (
-        <>
-            <Input name="store" placeholder="store Name (required)" />
-            <Input name="userName" placeholder="First Name (required)" />
-            <Input name="password" placeholder="Password (required)" />
-            <FormBtn name="submitBtn" >Sign Up</FormBtn>
-        </>
-    );
+class SignUp extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userName: "",
+            password: "",
+            storeName: "",
+            firstName: "",
+            lastName: "",
+            name: ""
+        }
+    }
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+
+        this.setState({
+            [name]: value
+        });
+    };
+
+    submitSignUp = (event) => {
+        event.preventDefault();
+        //send information to the user API
+        Axios.post("/api/users")
+        //when you get the response, redirect the user to the store page
+        console.log(this.state.userName + this.state.password + this.state.storeName);
+    }
+
+    render() {
+        return (
+            <>
+                <Input name="storeName" placeholder="store Name (required)" value={this.state.storeName} onChange={this.handleInputChange} />
+                <Input name="userName" placeholder="First Name (required)" className="form-control" value={this.state.userName} onChange={this.handleInputChange} />
+                <Input name="password" placeholder="Password (required)" className="form-control" value={this.state.password} onChange={this.handleInputChange} />
+                <FormBtn name="submitBtn" onClick={this.submitSignUp} >Sign Up</FormBtn>
+            </>
+        );
+    }
+
 }
 
 export default SignUp;
