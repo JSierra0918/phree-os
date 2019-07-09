6e9727073e350e2d0138dd04da235bd547adfe8d
Jorge Sierra <3d.jorgesierra@gmail.com>
2019-07-08 21:02:05 -0400

    fixes for stripe

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
           <Route exact path="/" component={LoginPage} />
           <Route exact path="/SignIn" component={SignIn} />
           <Route exact path="/SignUp" component={SignUp} />
-          <Route exact path="/store" component={StorePage} />
+          <Route  path="/store" component={StorePage} />
           <Route exact path="/manage" component={ManagePage} />
         </Switch>
       </Router>
