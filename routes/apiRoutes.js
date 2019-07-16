3676e4eeb139a486dae9703faac089c208c26c97
Jorge Sierra <3d.jorgesierra@gmail.com>
2019-07-16 17:19:48 -0400

    testing stripe

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -165,6 +165,7 @@ module.exports = function(app) {
   app.post("/api/stripe", function(req, res) {
     console.log("in stripe api call");
     console.log("req.body.userId: ", req.body.userId);
+    console.log(config.stripe_secret_key);
     request.post(
       {
         url: "https://connect.stripe.com/oauth/token",
