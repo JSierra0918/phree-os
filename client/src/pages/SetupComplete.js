2c2892b6a5cacbd180b3a5178fc58da0c3bacc10
Jorge Sierra <3d.jorgesierra@gmail.com>
2019-07-15 21:06:03 -0400

    re.json on hasStripe

--- client/src/pages/SetupComplete.js
+++ client/src/pages/SetupComplete.js
@@ -17,7 +17,7 @@ class SetupComplete extends Component {
         userId : data
     }
     API.postStripe(code)
-    setTimeout(() => {this.routeToStore()}, 10000)
+    setTimeout(() => {this.routeToStore()}, 1000)
   }
 
   routeToStore = () => {
